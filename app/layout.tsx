500b730c75477333b17cb80843e681147ee611d5
Vee <ifebuchejulia@gmail.com>
2024-01-25 20:51:09 +0100

    made some error fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+// import { Inter } from 'next/font/google'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -21,7 +21,7 @@ export default function RootLayout({
         <link rel="icon" href="/img/logo.svg" />
       </head>
       
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   )
 }
